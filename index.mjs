8a0b1961d06267e04692a60ef5e982181683bec3
Marcell Szikora <szikora.marcell@webness.hu>
2025-01-19 15:36:50 +0100

    using ses client well

--- index.mjs
+++ index.mjs
@@ -1,13 +1,15 @@
-import * as AWS from "@aws-sdk/client-ses";
+"use strict";
+
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
 // Initialize AWS SES
-const ses = new AWS.SES({ region: "REGION" });
+const ses = new SESClient({ region: "us-east-1" });
 
 export const handler = async (event) => {
   try {
     // Extract email addresses from the event payload
-    const senderEmail = event.senderEmail || "default-sender@example.com";
-    const recipientEmail = event.recipientEmail || "default-recipient@example.com";
+    const senderEmail = event.senderEmail || "default@sender.com";
+    const recipientEmail = event.recipientEmail || "default@recipient.com";
 
     // Extract SES event details
     const record = event.Records[0].ses;
@@ -26,20 +28,21 @@ export const handler = async (event) => {
         },
         Body: {
           Text: {
-            Data: `You've received an email forwarded from SES.\n\nOriginal Message ID: ${messageId}`,
+            Data: `You've received an email forwarded from SES.\n\nOriginal Message ID: ${messageId}\n\n`,
           },
         },
       },
     };
-
+    console.log(params);
     // Send the email via SES
-    await ses.send(params);
+    const sendingResult = await ses.send(new SendEmailCommand(params));
+    console.log(sendingResult);
 
-    console.log(`Email forwarded successfully! Message ID: ${messageId}`);
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Email forwarded successfully!" }),
+      body: JSON.stringify({ message: "Successfull sending.", messageId }),
     };
+    
   } catch (error) {
     console.error("Error forwarding email:", error);
     return {
